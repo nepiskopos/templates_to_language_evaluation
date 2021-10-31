64b989ccadb7811d7473353151f82ae1ca1c6500
Înbishdev <nepisko@gmail.com>
2021-10-31 03:08:27 +0200

    NTG: improve E2E evaluation bash script

--- e2e/e2e_metrics_ntg.sh
+++ e2e/e2e_metrics_ntg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]
+# Usage: ./e2e_metrics_ntg.sh -nar [or -war] -decay ['psql' (or see tabulate docs - only with -mean_stdev)]
 
 
 # Load bashrc
@@ -9,28 +9,14 @@ source /root/.bashrc
 
 
 # Check the corectness of the provided command-line arguments
-if [[ $1 != -decayed ]]; then
-    echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]"
+if [[ $1 != -nar && $1 != -war ]]; then
+    echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -war] ['psql' (or see tabulate docs - only with -mean_stdev)]"
     
     exit 1
-elif [[ $2 != 0 && $2 != 1 ]]; then
-    echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]"
+elif [[ $2 != -decay ]]; then
+    echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -war] ['psql' (or see tabulate docs - only with -mean_stdev)]"
     
     exit 1
-elif [[ $3 != -nar && $3 != -far ]]; then
-    echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]"
-    
-    exit 1
-elif [ ! -z $4 ]; then
-    if [[ $4 != -mean_stdev ]]; then
-        echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]"
-        
-        exit 1
-    elif [ -z "$5" ]; then
-        echo "Usage: ./e2e_metrics_ntg.sh -decayed 1 [or 0] -nar [or -far] [-mean_stdev] ['psql' (or see tabulate docs - only with -mean_stdev)]"
-        
-        exit 1
-    fi
 fi
 
 
@@ -38,50 +24,77 @@ fi
 source /root/miniconda3/etc/profile.d/conda.sh
 conda activate e2e_metrics
 
+# If decay option chosen
+dec=0
+if [ ! -z "$2" ]; then
+    dec=1
+fi
 
-if [[ $3 == -nar ]]; then
+
+
+if [[ $1 == -nar ]]; then
     # Evaluate non-autoregressive NTG outputs on the validation set
-    /root/e2e-metrics/measure_scores.py -l output_scores/complete_scores/ntg_valid_dec$2_scores.tsv \
+    /root/e2e-metrics/measure_scores.py -l /root/e2e-metrics/output_scores/complete_scores/ntg_valid_dec"$dec"_scores.tsv \
     -t -H /root/tgen/e2e-challenge/input/valid-conc.txt \
-    /root/neural-template-gen/gens_postprocessed/gen-e2e-55-5-valid-dec$2-postprocessed.txt
+    /root/neural-template-gen/gens_postprocessed/gen-e2e-55-5-valid-dec"$dec"-postprocessed.txt
     
     # Evaluate non-autoregressive NTG outputs on the test set
-    /root/e2e-metrics/measure_scores.py -l output_scores/complete_scores/ntg_test_dec$2_scores.tsv \
+    /root/e2e-metrics/measure_scores.py -l /root/e2e-metrics/output_scores/complete_scores/ntg_test_dec"$dec"_scores.tsv \
     -t -H /root/tgen/e2e-challenge/input/test-conc.txt \
-    /root/neural-template-gen/gens_postprocessed/gen-e2e-55-5-test-dec$2-postprocessed.txt
+    /root/neural-template-gen/gens_postprocessed/gen-e2e-55-5-test-dec"$dec"-postprocessed.txt
     
-    if [ ! -z "$4" ]; then
+    if [ ! -z "$3" ]; then
         # Export the mean and standard deviations of the scores of non-autoregressive 
         # NTG outputs on validation set
-        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_valid_dec$2_scores.tsv \
-        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_valid_dec$2_scores_final.csv -t -f $5
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_valid_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_valid_dec"$dec"_scores_final.csv -t -f $3
         
         # Export the mean and standard deviations of the scores of non-autoregressive 
         # NTG outputs on test set
-        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_test_dec$2_scores.tsv \
-        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_test_dec$2_scores_final.csv -t -f $5
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_test_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_test_dec"$dec"_scores_final.csv -t -f $3
+    else
+        # Export the mean and standard deviations of the scores of non-autoregressive 
+        # NTG outputs on validation set
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_valid_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_valid_dec"$dec"_scores_final.csv -t
+        
+        # Export the mean and standard deviations of the scores of non-autoregressive 
+        # NTG outputs on test set
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_test_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_test_dec"$dec"_scores_final.csv -t
     fi
-elif [[ $3 == -far ]]; then
+elif [[ $1 == -war ]]; then
     # Evaluate autoregressive NTG outputs on the validation set
-    /root/e2e-metrics/measure_scores.py -l output_scores/complete_scores/ntg_far_valid_dec$2_scores.tsv \
+    /root/e2e-metrics/measure_scores.py -l /root/e2e-metrics/output_scores/complete_scores/ntg_war_valid_dec"$dec"_scores.tsv \
     -t -H /root/tgen/e2e-challenge/input/valid-conc.txt \
-    /root/neural-template-gen/gens_postprocessed/gen-e2e-60-1-far-valid-dec$2-postprocessed.txt
+    /root/neural-template-gen/gens_postprocessed/gen-e2e-60-1-war-valid-dec"$dec"-postprocessed.txt
     
     # Evaluate autoregressive NTG outputs on the test set
-    /root/e2e-metrics/measure_scores.py -l output_scores/complete_scores/ntg_far_test_dec$2_scores.tsv \
+    /root/e2e-metrics/measure_scores.py -l /root/e2e-metrics/output_scores/complete_scores/ntg_war_test_dec"$dec"_scores.tsv \
     -t -H /root/tgen/e2e-challenge/input/test-conc.txt \
-    /root/neural-template-gen/gens_postprocessed/gen-e2e-60-1-far-test-dec$2-postprocessed.txt
+    /root/neural-template-gen/gens_postprocessed/gen-e2e-60-1-war-test-dec"$dec"-postprocessed.txt
 
-    if [ ! -z "$4" ]; then
+    if [ ! -z "$3" ]; then
         # Display and export the mean and standard deviations of the 
         # scores of autoregressive NTG outputs on the validation set
-        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_far_valid_dec$2_scores.tsv \
-        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_far_valid_dec$2_scores_final.csv -t -f $5
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_war_valid_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_war_valid_dec"$dec"_scores_final.csv -t -f $3
         
         # Display and export the mean and standard deviations of the 
         # scores of autoregressive NTG outputs on the test set
-        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_far_test_dec$2_scores.tsv \
-        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_far_test_dec$2_scores_final.csv -t -f $5
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_war_test_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_war_test_dec"$dec"_scores_final.csv -t -f $3
+    else
+        # Display and export the mean and standard deviations of the 
+        # scores of autoregressive NTG outputs on the validation set
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_war_valid_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_war_valid_dec"$dec"_scores_final.csv -t
+        
+        # Display and export the mean and standard deviations of the 
+        # scores of autoregressive NTG outputs on the test set
+        /root/e2e-metrics/display_scores.py /root/e2e-metrics/output_scores/complete_scores/ntg_war_test_dec"$dec"_scores.tsv \
+        -e /root/e2e-metrics/output_scores/mean_stdev_scores/ntg_war_test_dec"$dec"_scores_final.csv -t
     fi
 fi
 

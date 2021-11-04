d15bdd32fddb8104e01ac9b4d225868c3a8c6050
nbishdev <nepiskopos@fogus.gr>
2021-11-04 17:19:42 +0200

    NTG: Fix path to original authors' models

--- ntg/ntg_e2e_original.sh
+++ ntg/ntg_e2e_original.sh
@@ -54,7 +54,7 @@ if [[ $1 == -nar ]]; then
     # Generate on the E2E test set using the non-autoregressive model
     python2 /root/neural-template-gen/chsmm.py -data /root/neural-template-gen/data/e2e_aligned/ -emb_size 300 -hid_size 300 -layers 1 -K 55 -L 4 \
     -log_interval 200 -thresh 9 -emb_drop -bsz 15 -max_seqlen 55 -lr 0.5 -sep_attn -max_pool -unif_lenps -one_rnn -Kmul 5 -mlpinp -onmt_decay $gpu \
-    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_test.txt -load /root/neural-template-gen/original_models/models/e2e-55-5.pt \
+    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_test.txt -load /root/neural-template-gen/models/original_models/e2e-55-5.pt \
     -tagged_fi /root/neural-template-gen/segs/original_segs/seg-e2e-55-5.txt -beamsz 5 -ntemplates 100 -gen_wts '1,1' \
     > /root/neural-template-gen/gens/original_gens/gen-e2e-55-5-test.txt
     
@@ -67,14 +67,14 @@ elif [[ $1 == -war ]]; then
     # Generate on the E2E validation set using the autoregressive model
     python2 /root/neural-template-gen/chsmm.py -data /root/neural-template-gen/data/e2e_aligned/ -emb_size 300 -hid_size 300 -layers 1 -K 60 -L 4 \
     -log_interval 200 -thresh 9 -emb_drop -bsz 15 -max_seqlen 55 -lr 0.5 -sep_attn -max_pool -unif_lenps -one_rnn -Kmul 1 -mlpinp -onmt_decay $gpu \
-    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_uniq_valid.txt -load /root/neural-template-gen/original_models/models/e2e-60-1-war.pt \
+    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_uniq_valid.txt -load /root/neural-template-gen/models/original_models/e2e-60-1-war.pt \
     -tagged_fi /root/neural-template-gen/segs/original_segs/seg-e2e-60-1-war.txt -beamsz 5 -ntemplates 100 -gen_wts '1,1' \
     > /root/neural-template-gen/original_gens/gens/gen-e2e-60-1-war-valid.txt
 
     # Generate on the E2E test set using the autoregressive model
     python2 /root/neural-template-gen/chsmm.py -data /root/neural-template-gen/data/e2e_aligned/ -emb_size 300 -hid_size 300 -layers 1 -K 60 -L 4 \
     -log_interval 200 -thresh 9 -emb_drop -bsz 15 -max_seqlen 55 -lr 0.5 -sep_attn -max_pool -unif_lenps -one_rnn -Kmul 1 -mlpinp -onmt_decay $gpu \
-    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_test.txt -load /root/neural-template-gen/original_models/models/e2e-60-1-war.pt \
+    -gen_from_fi /root/neural-template-gen/data/e2e_aligned/src_test.txt -load /root/neural-template-gen/models/original_models/e2e-60-1-war.pt \
     -tagged_fi /root/neural-template-gen/segs/original_segs/seg-e2e-60-1-war.txt -beamsz 5 -ntemplates 100 -gen_wts '1,1' \
     > /root/neural-template-gen/original_gens/gens/gen-e2e-60-1-war-test.txt
 

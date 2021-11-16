a180be6ae735ba2a316b3deabc9733da46f1ae95
nbishdev <58558195+nbishdev@users.noreply.github.com>
2021-11-16 17:25:34 +0200

    Update ntg_wb_original.sh

--- ntg/ntg_wb_original.sh
+++ ntg/ntg_wb_original.sh
@@ -43,14 +43,14 @@ fi
 
 
 # Text Generation
-if [[ $2 == -nar ]]; then
+if [[ $1 == -nar ]]; then
     # Generate on the WikiBio test set using the non-autoregressive model
     python2 /root/neural-template-gen/chsmm.py -data /root/neural-template-gen/data/wb_aligned/ -emb_size 300 -hid_size 300 -layers 1 -K 45 -L 4 \
     -log_interval 1000 -thresh 29 -emb_drop -bsz 5 -max_seqlen 55 -lr 0.5 -sep_attn -max_pool -unif_lenps -one_rnn -Kmul 3 -mlpinp -onmt_decay $gpu \
     -gen_from_fi /root/neural-template-gen/data/wb_aligned/src_test.txt -load /root/neural-template-gen/models/original_models/wb-45-3.pt \
     -tagged_fi /root/neural-template-gen/segs/original_segs/seg-wb-45-3.txt -beamsz 5 -ntemplates 100 -gen_wts '1,1' -min_gen_tokes 20 \
     > /root/neural-template-gen/gens/original_gens/gen-wb-45-3.txt
-elif [[ $2 == -war ]]; then
+elif [[ $1 == -war ]]; then
     # Generate on the WikiBio test set using the autoregressive model
     python2 /root/neural-template-gen/chsmm.py -data /root/neural-template-gen/data/wb_aligned/ -emb_size 300 -hid_size 300 -layers 1 -K 45 -L 4 \
     -log_interval 1000 -thresh 29 -emb_drop -bsz 5 -max_seqlen 55 -lr 0.5 -sep_attn -max_pool -unif_lenps -one_rnn -Kmul 3 -mlpinp -onmt_decay $gpu \

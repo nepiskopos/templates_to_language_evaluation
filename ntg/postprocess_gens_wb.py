f6df3dfe0ebf1004aaf62e370220dd22481ef3be
Nikolaos Episkopos <58558195+nepiskopos@users.noreply.github.com>
2024-04-21 02:20:22 +0300

    Add postprocessing script for NTG WikiBio gens.

--- ntg/postprocess_gens_wb.py
+++ ntg/postprocess_gens_wb.py
@@ -28,6 +28,17 @@ class Detokenizer(object):
         self._contract = Regex(r" (\p{Alpha}+) ' (ll|ve|re|[dsmt])(?= )", flags=UNICODE | IGNORECASE)
         self._dash_fixes = Regex(r" (\p{Alpha}+|£ [0-9]+) - (priced|star|friendly|(?:£ )?[0-9]+) ", flags=UNICODE | IGNORECASE)
         self._dash_fixes2 = Regex(r" (non) - ([\p{Alpha}-]+) ", flags=UNICODE | IGNORECASE)
+        self._squotes1 = Regex(r" ` ", flags=UNICODE | IGNORECASE)
+        self._squotes2 = Regex(r" ' ", flags=UNICODE | IGNORECASE)
+        self._dquotes1 = Regex(r"`` `` ", flags=UNICODE | IGNORECASE)
+        self._dquotes2 = Regex(r" '' ''", flags=UNICODE | IGNORECASE)
+        self._quotes1 = Regex(r"`` ", flags=UNICODE | IGNORECASE)
+        self._quotes2 = Regex(r" ''", flags=UNICODE | IGNORECASE)
+        self._dlrb = Regex(r"-lrb- -lrb- ", flags=UNICODE | IGNORECASE)
+        self._drrb = Regex(r" -rrb- -rrb-", flags=UNICODE | IGNORECASE)
+        self._lrb = Regex(r"-lrb- ", flags=UNICODE | IGNORECASE)
+        self._rrb = Regex(r" -rrb-", flags=UNICODE | IGNORECASE)
+        self._dashes = Regex(r"--", flags=UNICODE | IGNORECASE)
 
     def detokenize(self, text):
         """\
@@ -39,6 +50,17 @@ class Detokenizer(object):
         text = self._currency_or_init_punct.sub(r' \1', text)
         text = self._noprespace_punct.sub(r'\1 ', text)
         text = self._contract.sub(r" \1'\2", text)
+        text = self._squotes1.sub(r' "', text)
+        text = self._squotes2.sub(r'" ', text)
+        text = self._dquotes1.sub(r'"', text)
+        text = self._dquotes2.sub(r'"', text)
+        text = self._quotes1.sub(r'"', text)
+        text = self._quotes2.sub(r'"', text)
+        text = self._dlrb.sub(r'(', text)
+        text = self._drrb.sub(r')', text)
+        text = self._lrb.sub(r'(', text)
+        text = self._rrb.sub(r')', text)
+        text = self._dashes.sub(r'-', text)
         text = text.strip()
         # capitalize
         if not text:
@@ -66,7 +88,7 @@ def process_file(input_file_name, output_file_name):
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Post-process NTG generated output files for the E2E challenge')
+    parser = ArgumentParser(description='Post-process NTG generated output files for the WikiBio challenge')
     parser.add_argument('input_file', type=str, help='input file (and output file, if in-place)')
     parser.add_argument('output_file', type=str, nargs='?', help='output file (if not in-place)')
     args = parser.parse_args()
